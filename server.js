5ce89e56ad811df8dd9412ea3f11ba065b92b6ba
dragonouou <shenglanouou@gmail.com>
2020-12-03 20:26:48 -0800

    initialize server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.get('/hello', function(req, res){
+    res.send('hello world'); });
+app.listen(3000);
+
+// app.use(function (req,res,next) {
+//     res.header('Access-Control-Allow-Origin',"*");
+//     res.header('Access-Control-Allow-Headers',
+//         'Content-Type, X-Requested_With, Origin');
+//     res.header('Access-Control-Allow-Methods',
+//         'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+//     next();
+// })
