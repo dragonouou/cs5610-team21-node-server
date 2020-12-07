0e0339d961ed81e407d016e675fc3a1924da04c7
Cynthia Chen <xinyi.chen@berkeley.edu>
2020-12-06 16:38:05 -0800

    users server updated

--- models/users/users.schema.server.js
+++ models/users/users.schema.server.js
@@ -21,6 +21,10 @@ const usersSchema = mongoose.Schema({
     recipes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "RecipeModel"
+    }],
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "OrderModel"
     }]
 }, {collection: "users"})
 
