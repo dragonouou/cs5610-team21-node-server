5ea1803ee280d31f62e0585699403b70c7422d0e
dragonouou <shenglanouou@gmail.com>
2020-12-10 13:07:37 -0800

    set origin

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ app.use(session({
 }));
 
 app.use(function (req,res,next) {
-    res.header('Access-Control-Allow-Origin',"https://homekitchen.herokuapp.com/");
+    const allowedOrigins = ['http://localhost:3000/','https://homekitchen.herokuapp.com/','http://localhost:3001/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)){
+        res.setHeader('Access-Control-Allow-Origin',origin);
+    }
+    // res.header('Access-Control-Allow-Origin',"https://homekitchen.herokuapp.com/");
     res.header('Access-Control-Allow-Credentials','true')
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested_With, Origin');
